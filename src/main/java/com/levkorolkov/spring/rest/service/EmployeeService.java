db708de3f54c04443cfa45c3cd8d3968b6c4c21a
LevKorolkov <lev.korolkov4@gmail.com>
2023-02-25 23:47:07 +0300

    Spring_Rest

---
+++ src/main/java/com/levkorolkov/spring/rest/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.levkorolkov.spring.rest.service;
+
+import com.levkorolkov.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+}
